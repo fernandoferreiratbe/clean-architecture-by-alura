4894f7e94bf24f58edf81e2dbd464bd6c29287f6
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2023-02-22 09:21:21 -0300

    Implement unit test for phone class

--- src/main/java/io/github/fernandoferreira/domain/Phone.java
+++ src/main/java/io/github/fernandoferreira/domain/Phone.java
@@ -1,11 +1,15 @@
 package io.github.fernandoferreira.domain;
 
+import java.util.Objects;
+
 public class Phone {
 
     private String ddd;
     private String number;
 
     public Phone(String ddd, String number) {
+        this.checkDddNumber(ddd);
+        this.checkPhoneNumber(number);
         this.ddd = ddd;
         this.number = number;
     }
@@ -16,5 +20,17 @@ public class Phone {
 
     public String getNumber() {
         return this.number;
+    }
+
+    private void checkDddNumber(String ddd) {
+        if (Objects.isNull(ddd) || !ddd.matches("[0-9]{2}")) {
+            throw new IllegalArgumentException("Invalid DDD number.");
+        }
+    }
+
+    private void checkPhoneNumber(String phoneNumber) {
+        if (Objects.isNull(phoneNumber) || !phoneNumber.matches("[0-9]{9}")) {
+            throw new IllegalArgumentException("Invalid phone number");
+        }
     }
 }
