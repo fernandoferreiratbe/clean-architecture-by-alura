03ddafaebf1a47ff00ff770f79fa03282f2a4aa8
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2023-03-02 08:22:35 -0300

    Update Recommendation domain object
    - Implement recommendation date time
    - Add Getters for all class attributes

--- src/main/java/io/github/fernandoferreira/domain/Recommendation.java
+++ src/main/java/io/github/fernandoferreira/domain/Recommendation.java
@@ -1,13 +1,28 @@
 package io.github.fernandoferreira.domain;
 
+import java.time.LocalDateTime;
+
 public class Recommendation {
 
     private Student indicated;
     private Student indicator;
+    private LocalDateTime recommendationDateTime;
 
     public Recommendation(Student indicated, Student indicator) {
         this.indicated = indicated;
         this.indicator = indicator;
+        this.recommendationDateTime = LocalDateTime.now();
     }
 
+    public Student getIndicated() {
+        return this.indicated;
+    }
+
+    public Student getIndicator() {
+        return this.indicator;
+    }
+
+    public LocalDateTime getRecommendationDateTime() {
+        return this.recommendationDateTime;
+    }
 }
