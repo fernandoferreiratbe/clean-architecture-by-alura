0bd9e99dcb11b41501636ae0fb583aea735e1f06
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2024-05-16 21:38:11 -0300

    refactor: Change Integer object instantiate

--- src/main/java/io/github/fernandoferreira/Main.java
+++ src/main/java/io/github/fernandoferreira/Main.java
@@ -1,31 +1,11 @@
 package io.github.fernandoferreira;
 
 public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello world!");
-//
-//        Integer num1 = 40;
-//        Integer num2 = 40;
-//        Integer num3 = 180;
-//        Integer num4 = 180;
-//
-//        if (num1 == num2)
-//            System.out.print("A");
-//        else
-//            System.out.print("B");
-//
-//        if (num3 == num4)
-//            System.out.print("C");
-//        else
-//            System.out.print("D");
-//    }
-
-        public static void main(String[] args) {
-            Integer num1 = new Integer(100);
-            Integer num2 = new Integer(100);
-            System.out.println(num1.hashCode());
-            System.out.print((num1 == num2)+":");
-            System.out.println(num1.equals(num2));
-        }
-
+    public static void main(String[] args) {
+        Integer num1 = Integer.valueOf("100");
+        Integer num2 = Integer.valueOf("100");
+        System.out.println(num1.hashCode());
+        System.out.print((num1.equals(num2))+":");
+        System.out.println(num1.equals(num2));
+    }
 }
